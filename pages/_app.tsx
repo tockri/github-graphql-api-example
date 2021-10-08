f7414877cbc15cb1dd5e3f9f5bbdf6fdf6de29c1
fujitama <fujita@nulab.com>
2021-10-08 18:06:17 +0900

    wip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import type {AppProps} from 'next/app'
 
 import * as React from 'react';
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
-import dynamic from "next/dynamic";
 
 // GraphQL クライアントを生成
 const apolloClient = new ApolloClient({
